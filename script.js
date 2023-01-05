0785984f393646b0c8a01142765fcb6d8b0db264
Steven Brooks <brookscodeblog@gmail.com>
2023-01-05 14:59:13 +0700

    Basic logic complete commit
    Basic functionality sorted. Now I need to build a master reset button and I also want to make the scoreboard more interactive. For example - display messages for the winner, then return to the score. I'm expecting this part to be troublesome. Once I've sorted all this, I can crack on with the styling - looks like shit right now!

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+// SCOREBOX
 const scoreBox = document.querySelector('.score-box');
-scoreBox.innerHTML = 'Player: 0 / Computer: 0';
-
+let playerScore = 0;
+let compScore = 0;
+scoreBox.innerHTML = `Player: ${playerScore} | Machine: ${compScore}`;
 
 // USER CHOICE
 const userChoice = document.querySelector('.user-choice');
@@ -8,45 +10,75 @@ const romeo = document.querySelector('.ch-romeo');
 const present = document.querySelector('.ch-present');
 const syphillis = document.querySelector('.ch-syphillis');
 const select = document.querySelector('.user-select');
-const userFinalChoice = '';
 
 romeo.addEventListener('click', romFunc);
 function romFunc() {
     userChoice.style.background = 'url(pics/romeo.png)';
     userChoice.style.backgroundSize = 'cover';
+    userChoice.children[0].innerHTML = 'romeo';
 }
 
 present.addEventListener('click', preFunc);
 function preFunc() {
     userChoice.style.background = 'url(pics/present.png)';
     userChoice.style.backgroundSize = 'cover';
+    userChoice.children[0].innerHTML = 'present';
 }
 
 syphillis.addEventListener('click', sypFunc);
 function sypFunc() {
     userChoice.style.background = 'url(pics/syphillis.jpg)';
     userChoice.style.backgroundSize = 'cover';
+    userChoice.children[0].innerHTML = 'syphillis';
 }
 
-select.addEventListener('click', selFunc);
-function selFunc() {
-    if(userChoice.style.background !== 'url(pics/romeo.png)') {
-        setTimeout(buttonFunc, 1000);
+select.addEventListener('click', selectFunc);
+function selectFunc() {
+    console.log(`User choice: ${userChoice.children[0].innerHTML}`);
+    if(userChoice.children[0].innerHTML.length > 0) {
+        setTimeout(compFunc, 1000);
     }
 }
 
-/*
-1. BUILD VARIABLE FOR RECORDING USER SELECTION
-2. BUILD COMP RANDOM SELECTION GENERATOR
-3. WRITE LOGIC TO DETERMINE WINNER
-4. BUILD SCOREBOX WITH TWO POINTS SECTIONS
-5. ADD POINT ACCORDINGLY
-*/
-
 // COMP CHOICE
-
-function buttonFunc() {
-    const choiceArr = ['romeo', 'picture', 'syphillis'];
-    const choiceIndex = Math.floor(Math.random() * choiceArr.length);
-    console.log(choiceArr[choiceIndex]);
+const compChoice = document.querySelector('.comp-choice');
+function compFunc() {
+    const compArr = ['romeo', 'present', 'syphillis'];
+    const compIndex = Math.floor(Math.random() * compArr.length);
+    compChoice.children[0].innerHTML = compArr[compIndex];
+    console.log(`Comp choice: ${compChoice.children[0].innerHTML}`);
+    setTimeout(determineFunc, 1000);
 }
+
+// DETERMINE WINNER & RESET
+function determineFunc() {
+    if((userChoice.children[0].innerHTML === 'romeo' && compChoice.children[0].innerHTML === 'romeo') ||
+        (userChoice.children[0].innerHTML === 'present' && compChoice.children[0].innerHTML === 'present') ||
+        (userChoice.children[0].innerHTML === 'syphillis' && compChoice.children[0].innerHTML === 'syphillis')) {
+            setTimeout(drawReset, 2000);
+        } else if((userChoice.children[0].innerHTML === 'romeo' && compChoice.children[0].innerHTML === 'present') ||
+                    (userChoice.children[0].innerHTML === 'present' && compChoice.children[0].innerHTML === 'syphillis') ||
+                    (userChoice.children[0].innerHTML === 'syphillis' && compChoice.children[0].innerHTML === 'romeo')) {
+                        playerScore++;
+                        scoreBox.innerHTML = `Player: ${playerScore} | Machine: ${compScore}`;
+                        setTimeout(winReset, 2000);
+                    } else {
+                        compScore++;
+                        scoreBox.innerHTML = `Player: ${playerScore} | Machine: ${compScore}`;
+                        setTimeout(winReset, 2000);
+                    }
+}
+
+function drawReset() {
+    userChoice.style.background = 'antiquewhite';
+    userChoice.children[0].innerHTML = '';
+}
+
+function winReset() {
+    userChoice.style.background = 'antiquewhite';
+    userChoice.children[0].innerHTML = '';
+}
+
+
+
+// once winner determined, user-choice returns to normal
